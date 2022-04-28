19683cd1402890b3b3c2914e14b891b1afd1eac8
y <y@DESKTOP-1E4JF9F>
2022-04-28 17:43:06 +0900

    save

---
+++ black/WebContent/resources/js/day04/join.js
@@ -0,0 +1,24 @@
+// join 버튼 클릭이벤트
+document.getElementById('jbtn').onclick = function(){
+	// 데이터 입력체크
+	var sname = document.getElementById('name').value;
+	if(!sname){
+		document.getElementById('name').focus();
+		return;
+	}
+	var sid = document.getElementById('id').value;
+	if(!sid){
+		document.getElementById('id').focus();
+		return;
+	}
+	var spw = document.getElementById('pw').value;
+	if(!sid){
+		document.getElementById('pw').focus();
+		return;
+	}
+}
+
+// reset 버튼 이벤트
+document.getElementById('rbtn').onclick = function(){
+	document.getElementById('frm').reset();
+}
