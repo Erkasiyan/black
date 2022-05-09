2946af6a2e02afcc0e6966e4de3a5b02f59a2795
y <y@DESKTOP-1E4JF9F>
2022-05-09 15:08:49 +0900

    save

--- black/WebContent/join/join.js
+++ black/WebContent/join/join.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
-	$("input[name='user']").change(function(){
+	$("input[name='']").change(function(){
+		$('#avtlabel').css('visibility','visible');
 		if(this.id == 'm'){
-			$('#mvt').css('visibility', 'visible');
+			$('#mvt').css('visibility','visible');
 			$('#fvt').css('visibility','hidden');
 		}
 		else if(this.id == 'f'){
@@ -14,14 +15,33 @@ $(document).ready(function(){
 $(document).ready(function(){
 		// 홈버튼 이벤트 처리
 		$('#hbtn').click(function(){
-			$(location).attr('href', '/black1/');			
+			$(location).attr('href', '/black/');			
 		});
 		// 리셋버튼 이벤트 처리
 		$('#rbtn').click(function(){
 			document.frm.reset();
-		});	
+		});
 	});
 
-document.getElementById('jbtn').onclick = function(){
-		document.frm.action="/black1/join/joinProc.html";
-}
+$(document).ready(function() {
+	$(document).ready(function(){
+		$('#ckmsg').css('display', 'none');
+		$('#pwck').keyup(function(){
+			var pw = $('#pw').val();
+			var pwck = $('#pwck').val();
+			
+			if(pw == pwck){
+				$('#pwck').removeAttr('name');
+				$('#ckmsg').html('*** 비밀번호가 일치합니다 ***').css('color', 'blue');
+				$('#ckmsg').css('display', 'block');
+			} else {
+				$('#ckmsg').html('*** 비밀번호가 다릅니다. ***').css('color', 'red');
+				$('#ckmsg').css('display', 'block');
+			}
+		});
+	});
+	
+	$('#jbtn').click(function () {
+  		$("#frm").submit();
+	});
+});
